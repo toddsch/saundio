9cdfd308407d4b40a3971b2bf9495ac716c2ec24
Todd <todd.scharien@gmail.com>
2018-10-10 22:30:02 -0700

    Begin Search implementation

--- app/src/main/java/website/todds/saundio/windows/MainActivity.java
+++ app/src/main/java/website/todds/saundio/windows/MainActivity.java
@@ -1,8 +1,14 @@
 package website.todds.saundio.windows;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,13 +17,18 @@ import android.view.MenuItem;
 import website.todds.saundio.R;
 import website.todds.saundio.util.BroadAction;
 import website.todds.saundio.windows.orderby.OrderByView;
+import website.todds.saundio.windows.search.SearchFragment;
 import website.todds.saundio.windows.tracks.TracksListFragment;
 import website.todds.toddlibs.andrutils.BroadcastUtil;
 import website.todds.toddlibs.andrutils.DialogUtil;
+import website.todds.toddlibs.andrutils.StrUtil;
 
 public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
 
     private Toolbar mToolbar;
+    private ConstraintLayout mLayout;
+    private ViewPager mPager;
+    private BroadcastReceiver mReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +36,43 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
         setContentView(R.layout.activity_main);
         setupToolbar();
 
+        mLayout = findViewById(R.id.main_layout);
+
         TracksListFragment tracks = (TracksListFragment)
                 Fragment.instantiate(this, TracksListFragment.class.getName());
 
         tracks.setLayoutManager(this, true, false);
 
-        ViewPager pager = findViewById(R.id.main_pager);
-        pager.setAdapter(new MainPagerAdapter(this, getSupportFragmentManager()));
+        mPager = findViewById(R.id.main_pager);
+        mPager.setAdapter(new MainPagerAdapter(this, getSupportFragmentManager()));
+
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                handleBroadcast(intent);
+            }
+        };
+        BroadcastUtil.regLocal(this, mReceiver, SearchFragment.BROADCAST_ACTION);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mToolbar.setOnMenuItemClickListener(null);
+        BroadcastUtil.unregLocal(this, mReceiver);
+    }
+
+    private void handleBroadcast(Intent intent) {
+        String action = intent.getAction();
+        if (action == null)
+            // No action given -- ignore broadcast
+            return;
+
+        if (action.equals(SearchFragment.BROADCAST_ACTION)) {
+            CharSequence seq = intent.getBooleanExtra(SearchFragment.KEY_STARTING, false) ?
+                    "" : getString(R.string.app_name);
+            mToolbar.setTitle(seq);
+        }
     }
 
     private void setupToolbar() {
@@ -45,7 +86,12 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_sort_rules:
+            case R.id.action_search: {
+                SearchFragment fragment = new SearchFragment();
+                fragment.show(getSupportFragmentManager(), SearchFragment.class.getName());
+                break;
+            }
+            case R.id.action_sort_rules: {
                 final OrderByView sortView = new OrderByView(this);
 
                 DialogInterface.OnClickListener okClick = new DialogInterface.OnClickListener() {
@@ -59,13 +105,17 @@ public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuIte
 
                 DialogUtil.viewDialog(this, sortView, okClick, null).show();
                 break;
+            }
         }
         return true;
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mToolbar.setOnMenuItemClickListener(null);
+    public void onBackPressed() {
+        FragmentManager manager = getSupportFragmentManager();
+        if (manager.getBackStackEntryCount() > 0)
+            manager.popBackStack();
+        else
+            super.onBackPressed();
     }
 }
---
+++ app/src/main/java/website/todds/saundio/windows/search/SearchFragment.java
@@ -0,0 +1,151 @@
+package website.todds.saundio.windows.search;
+
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+import website.todds.saundio.R;
+import website.todds.toddlibs.andrutils.BroadcastUtil;
+
+/**
+ * <p>A fragment used for searching the user's music library. Using
+ * {@link android.support.v4.content.LocalBroadcastManager Local broadcasts} this fragment will
+ * broadcast if it's entered {@link #onStart()} or {@link #onStop()} under the action
+ * {@link #BROADCAST_ACTION}.</p>
+ * <p>
+ * <p><tt>true</tt> will be set in the intent under {@link #KEY_STARTING} if
+ * in onStart(), <tt>false</tt> if in onStop().</p>
+ */
+public class SearchFragment extends DialogFragment {
+
+    public static final String BROADCAST_ACTION = SearchFragment.class.getName();
+    public static final String KEY_STARTING = "keyStarting";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Window window = getDialog().getWindow();
+        if (window == null)
+            return;
+
+        Resources resources = getResources();
+        Drawable transparentBg;
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP)
+            transparentBg = resources.getDrawable(R.drawable.background_translucent, null);
+        else
+            transparentBg = resources.getDrawable(R.drawable.background_translucent);
+
+        window.setBackgroundDrawable(transparentBg);
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        Intent intent = new Intent(BROADCAST_ACTION);
+        intent.putExtra(KEY_STARTING, true);
+        BroadcastUtil.broadcast(getContext(), intent);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Intent intent = new Intent(BROADCAST_ACTION);
+        intent.putExtra(KEY_STARTING, false);
+        BroadcastUtil.broadcast(getContext(), intent);
+    }
+}
+
+/*  Todd 2018-10-10
+
+    I really really REALLY wanted to implement some sort of background blurring for this fragment
+    but it was either computationally too expensive or it just didn't look good with only blurring
+    text. That being said, I'm not ready to throw away this code that I found online and modified:
+
+    ... implements ViewTreeObserver.OnPreDrawListener
+
+    private static final float SCALE_FACTOR = 1.0f;
+    private static final float RADIUS = 10.0f;
+
+    private Bitmap mBackground;
+
+    public void getBitmapFromView(View view) {
+        view.buildDrawingCache();
+        mBackground = Bitmap.createBitmap(view.getDrawingCache());
+    }
+
+    onViewCreated() {
+        ...
+        // Need to attach this to the tree so we can use it once it's been measured
+        view.getViewTreeObserver().addOnPreDrawListener(this);
+        ...
+    }
+
+    private void blur(final Bitmap bmp, final View view) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int width = (int) (view.getMeasuredWidth() / SCALE_FACTOR);
+                int height = (int) (view.getMeasuredHeight() / SCALE_FACTOR);
+
+                final Bitmap overlay = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                Canvas canvas = new Canvas(overlay);
+
+                canvas.translate(-view.getLeft(), -view.getTop());
+                canvas.scale(1 / SCALE_FACTOR, 1 / SCALE_FACTOR);
+
+                Paint paint = new Paint();
+                paint.setFlags(Paint.FILTER_BITMAP_FLAG);
+                canvas.drawBitmap(bmp, 0, 0, paint);
+
+                RenderScript rs = RenderScript.create(getActivity());
+                Allocation overlayAlloc = Allocation.createFromBitmap(rs, overlay);
+                ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, overlayAlloc.getElement());
+
+                blur.setInput(overlayAlloc);
+                blur.setRadius(RADIUS);
+                blur.forEach(overlayAlloc);
+
+                overlayAlloc.copyTo(overlay);
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        view.setBackground(new BitmapDrawable(getResources(), overlay));
+                    }
+                });
+
+                rs.destroy();
+            }
+        }).start();
+    }
+
+    @Override
+    public boolean onPreDraw() {
+        View view = getView();
+        if (view == null)
+            return false;
+
+        // Only blur this once
+        view.getViewTreeObserver().removeOnPreDrawListener(this);
+        blur(mBackground, view);
+        return true;
+    }
+ */
